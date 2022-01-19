fb063fae2e82ab73e844e05910272aa281ff450d
emmanuelthegeek <altshiftctrl6@gmail.com>
2022-01-19 18:16:54 +0100

    added the loop file

---
+++ loops.py
@@ -0,0 +1,11 @@
+# Task
+# The provided code stub reads and integer, n, from STDIN. For all non-negative integers i < n , print square of i.
+#
+# Example
+# n = 3
+# The list of non-negative integers that are less than n = 3 is [0, 1, 2]. Print the square of each number on a separate line.
+
+if __name__ == '__main__':
+    n = int(input())
+    for u in range(n):
+        print(u ** 2)
